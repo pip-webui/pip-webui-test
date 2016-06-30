3051eab28ac6289fe5d49f3bb313cd0f3786979f
Andrii Pidhirnyi <andrii_pidhirnyi@epam.com>
2016-06-30 14:30:09 +0300

    Update module dependencies and gulp tasks
    - add task for static js code analys
    - add dependecies for eslint
    - add eslint config file
    - establish version for packages into package.json
    - update scripts in package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,13 @@
-var 
+/* eslint-disable strict */
+
+var
     gulp = require('gulp'),
     concat = require('gulp-concat'),
-    less = require('gulp-less'),
     rename = require('gulp-rename'),
-    minifyCss = require('gulp-minify-css'),
     minifyJs = require('gulp-uglify'),
-    minifyHtml = require('gulp-minify-html'),
     ngAnnotate = require('gulp-ng-annotate'),
     sourceMaps = require('gulp-sourcemaps'),
-    ngHtml2Js = require('gulp-ng-html2js'),
-    jshint = require('gulp-jshint'),
+    eslint = require('gulp-eslint'),
     replace = require('gulp-replace'),
     addsrc = require('gulp-add-src'),
     es = require('event-stream'),
@@ -19,7 +17,7 @@ var
     parallelize = require('concurrent-transform'),
     pkg = require('./package.json');
 
-gulp.task('build-js', function() {
+gulp.task('build-js', function () {
     return gulp.src('./src/**/*.js')
         .pipe(ngAnnotate({ single_quotes: true, add: true, remove: true }))
         .pipe(addsrc('./build/' + pkg.name + '-html.js'))
@@ -29,22 +27,22 @@ gulp.task('build-js', function() {
         .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('build-js-min', function() {
+gulp.task('build-js-min', function () {
     return gulp.src('./src/**/*.js')
         .pipe(ngAnnotate({ single_quotes: true, add: true, remove: true }))
         .pipe(addsrc('./build/' + pkg.name + '-html.min.js'))
-        //.pipe(sourceMaps.init())
+        // .pipe(sourceMaps.init())
         .pipe(concat(pkg.name + '.min.js'))
         .pipe(minifyJs())
-        //.pipe(sourceMaps.write('.'))
+        // .pipe(sourceMaps.write('.'))
         .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('build-max', ['build-js']);
 gulp.task('build-min', ['build-js-min']);
 
-gulp.task('publish', function() {
-    var 
+gulp.task('publish', function () {
+    var
         publisher = awspublish.create({
             params: {
                 Bucket: 'webui.pipdevs.com'
@@ -53,22 +51,22 @@ gulp.task('publish', function() {
             secretAccessKey: 'Mtqe9QlWWgRZvS8AXaZqJXaG98BR3qq8gbJqeEk+',
             region: 'us-west-1'
         }),
-        
+
         distFiles = gulp.src([
-                './node_modules/pip-webui-lib/dist/**/*',
-                './node_modules/pip-webui-css/dist/**/*',
-                './node_modules/pip-webui-core/dist/**/*',
-                './node_modules/pip-webui-rest/dist/**/*',
-                './node_modules/pip-webui-controls/dist/**/*',
-                './node_modules/pip-webui-connected/dist/**/*',
-                './node_modules/pip-webui-pages/dist/**/*',
-                './node_modules/pip-webui-test/dist/**/*',
-                './dist/**/*',
-            ], { xbase: '.' }),
+            './node_modules/pip-webui-lib/dist/**/*',
+            './node_modules/pip-webui-css/dist/**/*',
+            './node_modules/pip-webui-core/dist/**/*',
+            './node_modules/pip-webui-rest/dist/**/*',
+            './node_modules/pip-webui-controls/dist/**/*',
+            './node_modules/pip-webui-connected/dist/**/*',
+            './node_modules/pip-webui-pages/dist/**/*',
+            './node_modules/pip-webui-test/dist/**/*',
+            './dist/**/*'
+        ], { xbase: '.' }),
 
         indexFiles = gulp.src([
-                './samples/**/index.*'
-            ], { xbase: '.' })
+            './samples/**/index.*'
+        ], { xbase: '.' })
             .pipe(replace(pkg.name + '-lib.css', pkg.name + '-lib.min.css'))
             .pipe(replace(pkg.name + '-lib.js', pkg.name + '-lib.min.js'))
             .pipe(replace(pkg.name + '.css', pkg.name + '.min.css'))
@@ -86,41 +84,42 @@ gulp.task('publish', function() {
             .pipe(replace('../../../dist/', '/' + pkg.name + '/'))
             .pipe(replace('../../dist/', '/' + pkg.name + '/'))
             .pipe(replace('../dist/', '/' + pkg.name + '/')),
-            
+
         otherFiles = gulp.src([
-                './samples/**/*',
-                '!./samples/**/index.*'
-            ], { xbase: '.' });
-            
+            './samples/**/*',
+            '!./samples/**/index.*'
+        ], { xbase: '.' });
+
     return es.merge([distFiles, indexFiles, otherFiles])
-        .pipe(rename(function(path) {
-            path.dirname = '/' + pkg.name + '/'+ path.dirname
+        .pipe(rename(function (path) {
+            path.dirname = '/' + pkg.name + '/' + path.dirname;
         }))
         .pipe(parallelize(publisher.publish(), 5))
         .pipe(publisher.sync(pkg.name))
         .pipe(awspublish.reporter());
 });
 
-gulp.task('jshint', function() {
-	return gulp.src('./src/**/*.js')
-		.pipe(jshint())
-		.pipe(jshint.reporter('default'));	
+gulp.task('lint', function () {
+    return gulp.src('./src/**/*.js')
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
 });
 
-gulp.task('clean', function() {
-    del(['./build', './dist']); 
+gulp.task('clean', function () {
+    del(['./build', './dist']);
 });
 
-gulp.task('watch', function() {
+gulp.task('watch', function () {
     gulp.watch('./src/**/*', ['rebuild']);
 });
 
-gulp.task('launch', function() {
+gulp.task('launch', function () {
     gulp.src(['.'])
         .pipe(webserver({
             port: 8010,
-            //livereload: false,
-            //directoryListing: false,
+            // livereload: false,
+            // directoryListing: false,
             open: 'http://localhost:8010/samples/index.html'
         }));
 });
