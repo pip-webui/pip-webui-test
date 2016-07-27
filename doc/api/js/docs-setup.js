3b0968dc6661726062f60bb4d99c52f795efc410
Anastas Fonotov <anastasf@gmail.com>
2016-07-27 18:22:27 +0300

    update docs

---
+++ doc/api/js/docs-setup.js
@@ -0,0 +1,38 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Reference"
+  },
+  "pages": [],
+  "apis": {
+    "api": true
+  },
+  "__file": "_FAKE_DEST_/js/docs-setup.js",
+  "__options": {
+    "startPage": "/api",
+    "scripts": [
+      "https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.3.20/angular.min.js",
+      "https://cdnjs.cloudflare.com/ajax/libs/marked/0.3.5/marked.js",
+      "https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.3.20/angular-animate.min.js"
+    ],
+    "styles": [],
+    "title": "API Reference",
+    "html5Mode": true,
+    "editExample": true,
+    "navTemplate": false,
+    "navContent": "",
+    "navTemplateData": {},
+    "loadDefaults": {
+      "angular": false,
+      "angularAnimate": false,
+      "marked": false
+    }
+  },
+  "html5Mode": true,
+  "editExample": true,
+  "startPage": "/api",
+  "scripts": [
+    "https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.3.20/angular.min.js",
+    "https://cdnjs.cloudflare.com/ajax/libs/marked/0.3.5/marked.js",
+    "https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.3.20/angular-animate.min.js"
+  ]
+};
